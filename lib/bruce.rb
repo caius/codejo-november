16f6853266c73e07f7bc4380a0a341c99ddb0892
Tom Bell <tomb@tombell.org.uk>
2011-11-29 07:38:01 -0500

    Finish implementation and tests

--- lib/bruce.rb
+++ lib/bruce.rb
@@ -1,8 +1,39 @@
 class Bruce
   VERSION = "0.0.1"
 
+  PICTURE_VALUES = {
+    :A => 14,
+    :K => 13,
+    :Q => 12,
+    :J => 11
+  }
+
+  PAIR_CATCH_PHRASE = "You get nothing for a pair, not in this game"
+
   def highest_card card1, card2
     return card1 if card2.nil?
     return card2 if card1.nil?
+
+    card1_value = card_value card1
+    card2_value = card_value card2
+
+    if card1_value > card2_value
+      card1
+    elsif card1_value == card2_value
+      PAIR_CATCH_PHRASE
+    else
+      card2
+    end
+
+  end
+
+  def card_value card
+    card = card[0...-1]
+
+    if PICTURE_VALUES.has_key? card.to_sym
+      PICTURE_VALUES[card.to_sym]
+    else
+      card.to_i
+    end
   end
 end
--- test/bruce_test.rb
+++ test/bruce_test.rb
@@ -13,11 +13,47 @@ class TestBruce < MiniTest::Unit::TestCase
   end
 
   def test_first_param_nil
-    assert_equal Bruce.new.highest_card(nil, "5H"), "5H"
+    assert_equal "5H", Bruce.new.highest_card(nil, "5H")
   end
 
   def test_second_param_nil
-    assert_equal Bruce.new.highest_card("2C", nil), "2C"
+    assert_equal "2C", Bruce.new.highest_card("2C", nil)
   end
 
+  def test_5H_and_2C_returns_5H
+    assert_equal "5H", Bruce.new.highest_card("5H", "2C")
+  end
+
+  def test_AH_and_10S_returns_AH
+    assert_equal "AH", Bruce.new.highest_card("AH", "10S")
+  end
+
+  def test_2D_and_QC_returns_QC
+    assert_equal "QC", Bruce.new.highest_card("2D", "QC")
+  end
+
+  def test_2H_and_2C_returns_catch_phrase
+    assert_equal "You get nothing for a pair, not in this game",
+      Bruce.new.highest_card("2H", "2C")
+  end
+
+  def test_card_value_of_2H
+    assert_equal 2, Bruce.new.card_value("2H")
+  end
+
+  def test_card_value_of_JC
+    assert_equal 11, Bruce.new.card_value("JC")
+  end
+
+  def test_card_value_of_QS
+    assert_equal 12, Bruce.new.card_value("QS")
+  end
+
+  def test_card_value_of_KH
+    assert_equal 13, Bruce.new.card_value("KH")
+  end
+
+  def test_card_value_of_AD
+    assert_equal 14, Bruce.new.card_value("AD")
+  end
 end
