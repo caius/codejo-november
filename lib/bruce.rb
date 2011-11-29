9ea84e8a94d2086c67010e43ac93b07accdae521
Tom Bell <tomb@tombell.org.uk>
2011-11-29 07:21:40 -0500

    add single params nil return card

--- lib/bruce.rb
+++ lib/bruce.rb
@@ -2,6 +2,7 @@ class Bruce
   VERSION = "0.0.1"
 
   def highest_card card1, card2
-    nil
+    return card1 if card2.nil?
+    return card2 if card1.nil?
   end
 end
--- test/bruce_test.rb
+++ test/bruce_test.rb
@@ -12,4 +12,12 @@ class TestBruce < MiniTest::Unit::TestCase
     assert_nil Bruce.new.highest_card nil, nil
   end
 
+  def test_first_param_nil
+    assert_equal Bruce.new.highest_card(nil, "5H"), "5H"
+  end
+
+  def test_second_param_nil
+    assert_equal Bruce.new.highest_card("2C", nil), "2C"
+  end
+
 end
