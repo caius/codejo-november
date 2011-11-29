5be9f5b8ae9a0cb2793795633c81ebfaa62cea92
Caius Durling <git@caius.name>
2011-11-29 23:12:05 +0000

    Refactor Bruce

--- lib/bruce.rb
+++ lib/bruce.rb
@@ -11,29 +11,18 @@ class Bruce
   PAIR_CATCH_PHRASE = "You get nothing for a pair, not in this game"
 
   def highest_card card1, card2
-    return card1 if card2.nil?
-    return card2 if card1.nil?
+    return card1 unless card2
+    return card2 unless card1
 
-    card1_value = card_value card1
-    card2_value = card_value card2
-
-    if card1_value > card2_value
-      card1
-    elsif card1_value == card2_value
-      PAIR_CATCH_PHRASE
-    else
-      card2
-    end
-
+    card1_value, card2_value = card_value(card1), card_value(card2)
+    
+    return card1 if card1_value > card2_value
+    return PAIR_CATCH_PHRASE if card1_value == card2_value
+    card2
   end
 
   def card_value card
-    card = card[0...-1]
-
-    if PICTURE_VALUES.has_key? card.to_sym
-      PICTURE_VALUES[card.to_sym]
-    else
-      card.to_i
-    end
+    PICTURE_VALUES[card.chop.to_sym] || card.to_i
   end
+
 end
